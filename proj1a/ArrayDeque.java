28b84c0e47d341ac90dfe9975d6d0dad06836691
Xianglin Zeng <Future_YL@outlook.com>
2024-08-10 01:24:02 +0800

    初步完成proj1a
    完成了双向队列的链表实现并通过了测试。
    完成了双向队列的数组实现，未进行测试。

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,319 @@
+public class ArrayDeque<T> {
+    private T[] data;
+    private int size;
+    // first and last are the index of the last inserted item.
+    private int first;
+    private int last;
+
+    private static final double useRate = 0.25;
+    private static final int factor = 2;
+    private static final int initSize = 8;
+
+    public ArrayDeque() {
+        this.data = (T[]) new Object[initSize];
+        this.first = -1;
+        this.last = -1;
+        this.size = 0;
+    }
+
+    public void addFirst(T item) {
+        if (this.size == 0) {
+            this.first = 3;
+            this.data[this.first] = item;
+            return;
+        }
+
+        if (size == this.data.length) {
+            T[] newData = (T[]) new Object[this.data.length * factor];
+            if (this.first < this.last) {
+                if (this.first != -1) {
+                    System.arraycopy(this.data, 0, newData, 0, this.data.length);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                    System.arraycopy(this.data, this.last + 1, newData, newData.length - (this.size - this.last - 1), this.size - this.last - 1);
+                    this.first = newData.length - (this.size - this.last - 1) - 1;
+                    this.data = newData;
+                    this.data[this.first] = item;
+                    this.size++;
+                    return;
+                }
+            }
+            else if (this.last != -1) {
+                int rightLength = this.size - this.last - 1;
+                System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                System.arraycopy(this.data, this.first,
+                        newData, newData.length - rightLength, rightLength);
+                this.first = newData.length - rightLength;
+            } else {
+                int rightLength = this.size - this.first;
+                System.arraycopy(this.data, 0, newData, 0, this.size - rightLength);
+                System.arraycopy(this.data, this.first, newData, newData.length - rightLength, rightLength);
+                this.first = newData.length - rightLength;
+            }
+            this.data = newData;
+            if (this.first == 0) {
+                this.first = this.data.length - 1;
+            } else {
+                this.first--;
+            }
+            this.data[this.first] = item;
+            this.size++;
+            return;
+        }
+
+        if (this.first == -1) {
+            int index = this.last - this.size + 1 >= 0 ?
+                    this.last - this.size + 1 : this.last + 1 + this.data.length - this.size;
+            this.first = index == 0 ? this.data.length - 1 : index - 1;
+            this.data[this.first] = item;
+            this.size++;
+            return;
+        }
+
+        if (this.first == 0) {
+            this.first = this.data.length - 1;
+        } else {
+            this.first--;
+        }
+        this.data[this.first] = item;
+        this.size++;
+    }
+
+    public void addLast(T item) {
+        if (this.size == 0) {
+            this.last = 4;
+            this.data[this.last] = item;
+            return;
+        }
+
+        if (size == this.data.length) {
+            T[] newData = (T[]) new Object[this.data.length * factor];
+            if (this.first < this.last && this.first != -1)
+                System.arraycopy(this.data, 0, newData, 0, this.data.length);
+            else if (this.first > this.last) {
+                if (this.last == -1) {
+                    System.arraycopy(this.data, 0, newData, 0, this.first);
+                    System.arraycopy(this.data, this.first, newData, newData.length - (this.size - this.first), this.size - this.first);
+                    this.last = this.first;
+                    this.first = newData.length - (this.size - this.first);
+                    this.data = newData;
+                    this.data[this.last] = item;
+                    this.size++;
+                    return;
+                } else {
+                    int rightLength = this.size - this.last - 1;
+                    System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                    System.arraycopy(this.data, this.first,
+                            newData, newData.length - (rightLength), rightLength);
+                }
+            } else {
+                int rightLength = this.size - this.last - 1;
+                System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                System.arraycopy(this.data, this.last + 1, newData, newData.length - rightLength, rightLength);
+            }
+            this.data = newData;
+            if (this.last == this.data.length - 1) {
+                this.last = 0;
+            } else {
+                this.last++;
+            }
+            this.data[this.last] = item;
+            this.size++;
+            return;
+        }
+
+        if (this.last == -1) {
+            int index = this.first + this.size - 1 <= this.data.length - 1 ?
+                    this.first + this.size - 1 : this.size - this.data.length + this.first - 1;
+            this.last = index == this.data.length - 1 ? 0 : index + 1;
+            this.data[this.last] = item;
+            this.size++;
+            return;
+        }
+
+        if (this.last == this.data.length - 1) {
+            this.last = 0;
+        } else {
+            this.last++;
+        }
+        this.data[this.last] = item;
+        this.size++;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void printDeque() {
+        if (this.size == 0) {
+            return;
+        }
+        if (this.first == -1) {
+            int index = this.last - this.size + 1 >= 0 ?
+                    this.last - this.size + 1 : this.last + 1 + this.data.length - this.size;
+            while (index <= this.last) {
+                System.out.print(this.data[index] + " ");
+                if (index == this.data.length - 1) {
+                    index = 0;
+                } else {
+                    index++;
+                }
+            }
+        }
+        if (this.last == -1) {
+            int index = this.first;
+            for (int i = 0; i < this.size; i++) {
+                System.out.print(this.data[index] + " ");
+                if (index == this.data.length - 1) {
+                    index = 0;
+                } else {
+                    index++;
+                }
+            }
+        }
+        int curIndex = this.first;
+        while (true) {
+            System.out.print(this.data[curIndex] + " ");
+            if (curIndex == this.last)
+                break;
+
+            if (curIndex == this.data.length - 1) {
+                curIndex = 0;
+            } else {
+                curIndex++;
+            }
+        }
+    }
+
+    public T removeFirst() {
+        if (this.size == 0) {
+            return null;
+        }
+        if (this.first == -1) {
+            int index = this.last - this.size + 1 >= 0 ?
+                    this.last - this.size + 1 : this.last + 1 + this.data.length - this.size;
+            this.size--;
+
+            if (this.data.length >= 16 && 1.0 * this.size / this.data.length < useRate) {
+                T[] newData = (T[]) new Object[this.data.length / factor];
+                if (this.last - this.size + 1 >= 0) {
+                    System.arraycopy(this.data, index + 1, newData, 0, this.size);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                    System.arraycopy(this.data, index + 1, newData, newData.length - (this.size - this.last), this.size - this.last);
+                }
+                this.data = newData;
+            }
+            return this.data[index];
+        }
+        int index = this.first;
+        if (this.first == this.data.length - 1) {
+            this.first = 0;
+        } else {
+            this.first++;
+        }
+        this.size--;
+
+        if (this.data.length >= 16 && 1.0 * this.size / this.data.length < useRate) {
+            T[] newData = (T[]) new Object[this.data.length / factor];
+            if (this.last == -1) {
+                int idx = this.first + this.size - 1 <= this.data.length - 1 ?
+                        this.first + this.size - 1 : this.size - this.data.length + this.first - 1;
+                if (this.first + this.size - 1 <= this.data.length - 1) {
+                    System.arraycopy(this.data, this.first, newData, 0, this.size);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, idx + 1);
+                    System.arraycopy(this.data, this.first, newData, newData.length - (this.size - idx - 1), this.size - idx - 1);
+                }
+            } else {
+                if (this.first < this.last) {
+                    System.arraycopy(this.data, this.first, newData, 0, this.size);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                    System.arraycopy(this.data, this.first, newData, newData.length - (this.size - this.last - 1), this.size - this.last - 1);
+                }
+            }
+            this.data = newData;
+        }
+        return this.data[index];
+    }
+
+    public T removeLast() {
+        if (this.size == 0) {
+            return null;
+        }
+        if (this.last == -1) {
+            int index = this.first + this.size - 1 <= this.data.length - 1 ?
+                    this.first + this.size - 1 : this.size - this.data.length + this.first - 1;
+            this.size--;
+
+            if (this.data.length >= 16 && 1.0 * this.size / this.data.length < useRate) {
+                T[] newData = (T[]) new Object[this.data.length / factor];
+                if (this.first + this.size - 1 <= this.data.length - 1) {
+                    System.arraycopy(this.data, index + 1, newData, 0, this.size);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, index);
+                    System.arraycopy(this.data, this.first, newData, newData.length - (this.size - index), this.data.length - index);
+                }
+                this.data = newData;
+            }
+            return this.data[index];
+        }
+        int index = this.last;
+        if (this.last == 0) {
+            this.last = this.data.length - 1;
+        } else {
+            this.last--;
+        }
+        this.size--;
+
+        if (this.data.length >= 16 && 1.0 * this.size / this.data.length < useRate) {
+            T[] newData = (T[]) new Object[this.data.length / factor];
+            if (this.first == -1) {
+                int idx = this.last - this.size + 1 >= 0 ?
+                        this.last - this.size + 1 : this.last + 1 + this.data.length - this.size;
+                if (this.last - this.size + 1 >= 0) {
+                    System.arraycopy(this.data, idx, newData, 0, this.size);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                    System.arraycopy(this.data, idx, newData, newData.length - (this.size - this.last - 1), this.size - this.last - 1);
+                }
+            } else {
+                if (this.first < this.last) {
+                    System.arraycopy(this.data, this.first, newData, 0, this.size);
+                } else {
+                    System.arraycopy(this.data, 0, newData, 0, this.last + 1);
+                    System.arraycopy(this.data, this.first, newData, newData.length - (this.size - this.last - 1), this.size - this.last - 1);
+                }
+            }
+            this.data = newData;
+        }
+
+        return this.data[index];
+    }
+
+    public T get(int index) {
+        if (index > this.size) {
+            return null;
+        }
+        if (this.first == -1) {
+            int start = this.last - this.size + 1 >= 0 ?
+                    this.last - this.size + 1 : this.last + 1 + this.data.length - this.size;
+            if (start + index <= this.data.length - 1) {
+                return this.data[start + index];
+            } else {
+                return this.data[index - (this.data.length - 1 - start) - 1];
+            }
+        }
+
+        if (this.first + index <= this.data.length - 1) {
+            return this.data[this.first + index];
+        } else {
+            return this.data[index - (this.data.length - 1 - this.first) - 1];
+        }
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,123 @@
+public class LinkedListDeque<T> {
+    private final Node<T> head;
+    private final Node<T> tail;
+    private int size;
+
+    public LinkedListDeque() {
+        this.head = new Node<>(null);
+        this.head.prev = null;
+        this.tail = new Node<>(null);
+        this.tail.next = null;
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.size = 0;
+    }
+
+    private static class Node<K> {
+        private final K item;
+        private Node<K> next;
+        private Node<K> prev;
+
+        public Node(K data) {
+            this.item = data;
+        }
+
+        public K getRecursiveHelper(int index, Node<K> current) {
+            if (index == 0) {
+                return this.item;
+            } else {
+                current = current.next;
+                return getRecursiveHelper(index - 1, current);
+            }
+        }
+    }
+
+    public void addFirst(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (this.size == 0) {
+            this.tail.prev = newNode;
+        }
+        newNode.next = this.head.next;
+        newNode.prev = this.head;
+        this.head.next.prev = newNode;
+        this.head.next = newNode;
+        this.size++;
+    }
+
+    public void addLast(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (this.size == 0) {
+            this.head.next = newNode;
+        }
+        newNode.next = this.tail;
+        newNode.prev = this.tail.prev;
+        this.tail.prev.next = newNode;
+        this.tail.prev = newNode;
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void printDeque() {
+        Node<T> cur = this.head.next;
+        while (cur != this.tail) {
+            System.out.print(cur.item + " ");
+            cur = cur.next;
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public T removeFirst() {
+        if (this.size == 0) {
+            return null;
+        }
+        Node<T> toRemoved = this.head.next;
+        T item = toRemoved.item;
+        this.head.next = toRemoved.next;
+        toRemoved.next.prev = this.head;
+        toRemoved.next = null;
+        toRemoved.prev = null;
+        this.size--;
+        return item;
+    }
+
+    public T removeLast() {
+        if (this.size == 0) {
+            return null;
+        }
+        Node<T> toRemoved = this.tail.prev;
+        T item = toRemoved.item;
+        this.tail.prev = toRemoved.prev;
+        toRemoved.prev.next = this.tail;
+        toRemoved.prev = null;
+        toRemoved.next = null;
+        this.size--;
+        return item;
+    }
+
+    public T get(int index) {
+        if (this.size - 1 < index) {
+            return null;
+        }
+        int counter = 0;
+        Node<T> cur = this.head.next;
+        while (counter != index) {
+            cur = cur.next;
+            counter++;
+        }
+        return cur.item;
+    }
+
+    public T getRecursive(int index) {
+        if (this.size - 1 < index) {
+            return null;
+        }
+        Node<T> cur = this.head.next;
+        return cur.getRecursiveHelper(index, cur);
+    }
+}
